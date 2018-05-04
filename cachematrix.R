900793d97818d424914798736ed548ab56728995
linus <linusfox5@gmail.com>
2018-05-04 13:03:45 -0600

    commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The overal function takes a value for matrix and stores inverse of that matrix in the cache
 
-## Write a short comment describing this function
+## this function takes in a matrix and stores the inverse in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  n <- NULL
+  set <- function(matrix){
+    x <<- matrix
+    n <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) n <<- inverse
+  getinverse <- function() n
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function checks the cache if the is a data to use for matrix if not it creates one and stores it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  n <- x$getinverse()
+  if (!is.null(n)){
+    message("getting cached data")
+    return(n)
+  }
+  data <- x$get()
+  n <- solve(data, ...)
+  x$setinverse(n)
+  n
 }
